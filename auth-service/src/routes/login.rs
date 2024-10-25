bff009bf29a16f75f7d6171555c744f1e18e111c
Akinyele Cafe-Febrissy <me@akinyele.ca>
2024-10-25 15:01:25 -0400

    [sprint2][task2] Implement log-in route success case

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,4 +1,5 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -8,6 +9,7 @@ use crate::{
         error::AuthAPIError,
         user::{Email, Password},
     },
+    utils::auth::generate_auth_cookie,
 };
 
 use super::utils::map_user_store_error_to_api_error;
@@ -20,10 +22,33 @@ pub struct LoginRequest {
 
 pub async fn login(
     State(state): State<AppState>,
+    jar: CookieJar,
     Json(login_request): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
     // let email = Email::parse(login_request.email);
+    let cloned_jar = jar.clone();
+    let user_validation_result = validate_login_request(state, login_request)
+        .await
+        .map(|email| match generate_auth_cookie(&email) {
+            Ok(auth_cookie) => {
+                let updated_jar = cloned_jar.add(auth_cookie);
+                updated_jar
+            }
+            Err(_) => cloned_jar,
+        });
+
+    match user_validation_result {
+        Err(e) => (jar, Err(e)),
+        Ok(jar) => (jar, Ok(StatusCode::OK.into_response())),
+    }
+}
+
+async fn validate_login_request(
+    state: AppState,
+    login_request: LoginRequest,
+) -> Result<Email, AuthAPIError> {
     let LoginRequest { email, password } = login_request;
+
     let email = Email::parse(email).map_err(map_user_store_error_to_api_error)?;
     let password = Password::parse(password).map_err(map_user_store_error_to_api_error)?;
 
@@ -33,15 +58,15 @@ pub async fn login(
         .validate_user(email.to_owned(), password)
         .map_err(map_user_store_error_to_api_error)?;
 
-    if let Err(error) = user_store.get_user(email) {
+    if let Err(error) = user_store.get_user(email.to_owned()) {
         match error {
             UserStoreError::InvalidCredentials(_) => {
                 return Err(AuthAPIError::IncorrectCredentials)
             }
             UserStoreError::UserNotFound => return Err(AuthAPIError::IncorrectCredentials),
             _ => return Err(AuthAPIError::UnexpectedError),
-        }
+        };
     }
 
-    Ok(StatusCode::OK.into_response())
+    Ok(email)
 }
---
+++ auth-service/src/utils/constants.rs
@@ -0,0 +1,22 @@
+use std::env as std_env;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref JWT_SECRET: String = set_token();
+}
+
+pub const JWT_COOKIE_NAME: &str = "jwt";
+
+pub mod env {
+    pub const JWT_SECRET_ENV_VAR: &str = "JWT_SECRET";
+}
+
+fn set_token() -> String {
+    dotenvy::dotenv().ok();
+    let secret = std_env::var(env::JWT_SECRET_ENV_VAR).expect("JWT_SECRET must be set.");
+    if secret.is_empty() {
+        panic!("JWT_SECRET must not be empty.");
+    }
+    secret
+}
