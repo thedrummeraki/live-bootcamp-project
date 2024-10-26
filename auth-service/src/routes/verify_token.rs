2fc19e170235a388e184821e958c401c522474d0
Akinyele Cafe-Febrissy <me@akinyele.ca>
2024-10-26 13:23:22 -0400

    [sprint3][task4] Implement verify-token route

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,18 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::utils::auth::validate_token;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct VerifyTokenResquest {
+    token: String,
+}
+
+pub async fn verify_token(Json(body): Json<VerifyTokenResquest>) -> impl IntoResponse {
+    let VerifyTokenResquest { token } = body;
+
+    match validate_token(&token).await {
+        Err(_) => StatusCode::UNAUTHORIZED.into_response(),
+        Ok(_) => StatusCode::OK.into_response(),
+    }
 }
--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,10 +1,34 @@
+use auth_service::{domain::user::Email, utils::auth::generate_auth_cookie};
+use serde_json::json;
+
 use crate::helpers::{ResponseExt, TestApp};
 
 #[tokio::test]
-async fn verify_token_should_return_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
+    let response = app.post_verify_token(&"invalidtoken").await;
 
-    let response = app.post_verify_token().await;
+    assert_eq!(response.status_code(), 422)
+}
 
-    assert_eq!(response.status_code(), 200);
+#[tokio::test]
+async fn should_return_200_valid_token() {
+    let app = TestApp::new().await;
+    let email = Email::parse("valid@email.com").unwrap();
+    let cookie = generate_auth_cookie(&email).unwrap();
+    let token = cookie.value().to_owned();
+
+    let response = app.post_verify_token(&json!({"token": token})).await;
+
+    assert_eq!(response.status_code(), 200)
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+    let response = app
+        .post_verify_token(&json!({"token": "invalidtoken"}))
+        .await;
+
+    assert_eq!(response.status_code(), 401)
 }
